20040d1165c04e25c68c56fc6740fc74a65781cd
MAISHAN NADIS <sindeed222@gmail.com>
2025-11-14 11:27:34 +0600

    Add login page and update components

--- frontend/src/routes/RouterIndex.tsx
+++ frontend/src/routes/RouterIndex.tsx
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router";
-import Home from "../pages/Home";
+import { Route, Routes, Navigate } from "react-router";
+import Login from "../pages/Login";
 import Layout from "../components/common/Layout";
 
 const RouterIndex = () => {
     return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="*" element={<div>Path not found</div>} />
-            </Routes>
-        </Layout>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/login" replace />} />
+                        <Route path="*" element={<div>Path not found</div>} />
+                    </Routes>
+                </Layout>
+            } />
+        </Routes>
     );
 };
 
